ab3857bd051aa9167598728dfbff2549c315613d
Zerooaa <quiaponmarksuu@gmail.com>
2024-06-11 23:20:38 +0800

    Creating Navbar Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LoginListComponent } from './login/login-list/login-list.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    LoginComponent,
+    LoginListComponent,
+      LogoutComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
